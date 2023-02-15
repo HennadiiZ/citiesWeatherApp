b22e07cef8c5a532a3cd66034151d1663163d4a4
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-02-15 11:09:19 +0200

    small update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,3 +5,4 @@ export const JSON_add = '.json';
 export const URL_city = 'http://api.openweathermap.org/data/2.5/weather?q=';
 export const API_KEY = 'f9a8afece24fc95713882fe8f164ee19';
 // export const API_KEY = 'e07899b8ac29022c34d61a58f51cd3db';
+//
