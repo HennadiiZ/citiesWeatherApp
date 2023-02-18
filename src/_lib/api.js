d5ead8f20ad947d5ac1cb681758af179c1a0ccb8
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-02-18 17:36:29 +0200

    init save

--- src/_lib/api.js
+++ src/_lib/api.js
@@ -1,26 +1,4 @@
 import { API_KEY, CITIES_fb, URL_city, JSON_add } from '../constants/constants';
-//1
-// export const fetchCities = async (setLoadedData, setIsLoading) => {
-//   try {
-//     const response = await fetch(`${CITIES_fb}`);
-//     if (!response.ok) {
-//       throw new Error("Cities not found");
-//     }
-//     const data = await response.json();
-  
-//     const listOfCities = [];
-//     for(const key in data) {
-//       listOfCities.push(data[key]);
-//     }
-//     setIsLoading(false);
-//     setLoadedData(listOfCities);
-//   } catch (error) {
-//     console.error("Error:", error.message);
-//   }
-// };
-
-
-
 
 
 export const fetchCities = async (setLoadedData, setIsLoading) => {
@@ -33,16 +11,11 @@ export const fetchCities = async (setLoadedData, setIsLoading) => {
   
     const listOfCities = [];
 
-    
-
     for(const key in data) {
-      // console.log("+++", {itemId: key, ...data[key]});
-      // listOfCities.push(data[key]);
       listOfCities.push({itemId: key, ...data[key]});
     }
     setIsLoading(false);
     setLoadedData(listOfCities);
-    // console.log(listOfCities);
   } catch (error) {
     console.error("Error:", error.message);
   }
@@ -50,10 +23,6 @@ export const fetchCities = async (setLoadedData, setIsLoading) => {
 
 
 
-
-
-
-//2
 export function fetchCityByName(city, setLoadedData) {
   fetch(`${URL_city}${city}&appid=${API_KEY}`)
   .then((response) => {
@@ -74,7 +43,8 @@ export function fetchCityByName(city, setLoadedData) {
     console.error(error);
   });
 };
-//3
+
+
 export function addCityToList(cityData) {
   fetch(
     `${CITIES_fb}${JSON_add}`,
@@ -95,3 +65,83 @@ export function addCityToList(cityData) {
     console.error(error);
   });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { API_KEY, CITIES_fb, URL_city, JSON_add } from '../constants/constants';
+
+
+// export const fetchCities = async (setLoadedData, setIsLoading) => {
+//   try {
+//     const response = await fetch(`${CITIES_fb}${JSON_add}`);
+//     if (!response.ok) {
+//       throw new Error("Cities not found");
+//     }
+//     const data = await response.json();
+  
+//     const listOfCities = [];
+
+//     for(const key in data) {
+//       listOfCities.push({itemId: key, ...data[key]});
+//     }
+//     setIsLoading(false);
+//     setLoadedData(listOfCities);
+//   } catch (error) {
+//     console.error("Error:", error.message);
+//   }
+// };
+
+
+
+// export function fetchCityByName(city, setLoadedData) {
+//   fetch(`${URL_city}${city}&appid=${API_KEY}`)
+//   .then((response) => {
+
+//     if (!response.ok) {
+//       throw new Error("City not found");
+//     }
+
+//     return response.json()
+//   })
+//   .then((data) => {
+//     const CITIES_DATA = []
+//     CITIES_DATA.push(data);
+//     setLoadedData(CITIES_DATA);
+//   })
+//   .catch(error => {
+//     console.log('error');
+//     console.error(error);
+//   });
+// };
+
+
+// export function addCityToList(cityData) {
+//   fetch(
+//     `${CITIES_fb}${JSON_add}`,
+//     {
+//       method: 'POST',
+//       body: JSON.stringify(cityData),
+//       headers: {'Content-Type': 'application/json'}
+//     }
+//   )
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Dreadful error");
+//     }
+//     return response.json();
+//   })
+//   .catch(error => {
+//     console.log('error !!!!!!');
+//     console.error(error);
+//   });
+// };
--- src/_store/data-context.js
+++ src/_store/data-context.js
@@ -43,3 +43,73 @@ export const DataContextProvider = (props) => {
 }
 
 export default DataContext;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+
+// const DataContext = React.createContext({
+//   cities: [],
+//   totalCities: 0,
+//   addCity: (chosenCity) => {},
+//   removeCity: (id) => {},
+//   itemIsSelected: (id) => {}
+// });
+
+// export const DataContextProvider = (props) => {
+//   const [addedCities, setAddedCities] = useState([]);
+
+//   const addCityHandler = (chosenCity) => {
+//     setAddedCities((prevCity) => {
+//       return prevCity.concat(chosenCity);
+//     });
+//   };
+
+//   const removeCityHandler = (itemId) => {
+//     setAddedCities((prevCity) => {
+//       return prevCity.filter(item => item.id !== itemId);
+//     });   
+//   };
+
+//   const itemIsSelectedHandler = (itemId) => {
+//     return addedCities.some(item => item.id === itemId);
+//   };
+
+//   const context= {
+//     cities: addedCities,
+//     totalCities: addedCities.length,
+//     addCity: addCityHandler,
+//     removeCity: removeCityHandler,
+//     itemIsSelected: itemIsSelectedHandler
+//   };
+      
+//   return (
+//     <DataContext.Provider value={context}>
+//       { props.children }
+//     </DataContext.Provider>
+//   );
+// }
+
+// export default DataContext;
