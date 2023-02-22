ece100a5513834231f32b3390e02b7a9b3010802
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-02-22 18:36:42 +0200

    update

--- src/_helpers/helpers.js
+++ src/_helpers/helpers.js
@@ -1,6 +1,7 @@
 export function convertedTime(city_dt) {
-  const unix_timestamp = city_dt;
-  const date = new Date(unix_timestamp * 1000);
+  // console.log(city_dt); //-----------------------
+  // const unix_timestamp = city_dt;
+  const date = new Date(city_dt * 1000);
   const hours = date.getHours();
 
   let minutes = date.getMinutes();
